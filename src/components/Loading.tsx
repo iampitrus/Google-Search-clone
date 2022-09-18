cb957216fc0db73bea9d574bb17f1fe29c58879c
Ogbonnaya Peter <ogbonnapete210@gmail.com>
2022-09-18 02:21:43 +0100

    LOader

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,18 @@
-import Loader from "react-loader-spinner";
+import { Puff } from "react-loader-spinner";
 
 function Loading() {
   return (
     <div className="flex justify-center items-center">
-      <Loader type="Puff" color="#00BFFF" height={550} width={80} />
+      <Puff
+        height="550"
+        width="80"
+        radius={1}
+        color="#00BFFF"
+        ariaLabel="puff-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
     </div>
   );
 }
